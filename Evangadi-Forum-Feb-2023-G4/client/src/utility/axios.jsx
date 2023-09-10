bf91249935df9bb0182fe2997d5cf7fb70a325f6
Edil-Mg <135086322+Edil-Mg@users.noreply.github.com>
2023-09-09 21:40:47 -0400

    axios changed

--- Evangadi-Forum-Feb-2023-G4/client/src/utility/axios.jsx
+++ Evangadi-Forum-Feb-2023-G4/client/src/utility/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://nice-pink-yak-shoe.cyclic.cloud/",
+	baseURL: "https://e-forum.onrender.com",
 	// baseURL: "http://localhost:4500", // api url
 	// baseURL: "https://clean-gloves-bee.cyclic.app", // api url
 	// baseURL: "https://vast-plum-scallop-slip.cyclic.app",
